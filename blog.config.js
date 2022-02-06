85150b2a245496ab86ab5c8159b12c6d216ac88d
Yanng <719398840@qq.com>
2022-02-06 15:00:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://pbs.twimg.com/profile_images/1441255815915081729/GUp0h1Rn_400x400.jpg', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/94e0c14d-e3fc-47c3-ba7c-28f8d4183bca/FBFD8590-29EF-49BC-ACE7-13E2B02D3A6D.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220206%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220206T065941Z&X-Amz-Expires=86400&X-Amz-Signature=61155275513f3221c04d921e85ff0f74cd5d06a1dc4baa399203fc917a19f094&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22FBFD8590-29EF-49BC-ACE7-13E2B02D3A6D.jpeg%22&x-id=GetObject', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/yanng961',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
