a4f32635ca48bbb090b4c0e5c6d85d8c68e79a51
Yanng <719398840@qq.com>
2023-02-19 00:12:35 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import { useLocale } from '@/lib/locale'
 const NavBar = () => {
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
+    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: false },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
